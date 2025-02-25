853cd18714bfd397a6721cfac2b683274ce64958
EgnalyGonz <EXGC0003@ce.pucmm.edu.do>
2025-02-25 16:21:37 -0400

    Agregue los cambios a la clase parada, para que ahora maneje los distintos tipos

--- Mapa/Parada.java
+++ Mapa/Parada.java
@@ -1,4 +1,7 @@
-public class Parada
+import java.util.HashSet;
+import java.util.Set;
+
+/*public class Parada
 {
     private String nombre;
 
@@ -14,4 +17,47 @@ public class Parada
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-}
+    */
+
+        public class Parada {
+
+            private String nombre;
+            private Set<String> tiposDeTransporte; // agregue esto asi podemos manejar los tipos de transporte que habiamos hablado
+
+            public Parada(String nombre) {
+                this.nombre = nombre;
+                this.tiposDeTransporte = new HashSet<>();
+            }
+
+            public String getNombre() {
+                return nombre;
+            }
+
+            public void setNombre(String nombre) {
+                this.nombre = nombre;
+            }
+
+            public void agregarTransporte(String tipo) {
+                tiposDeTransporte.add(tipo);
+            }
+
+            public void eliminarTransporte(String tipo) {
+                tiposDeTransporte.remove(tipo);
+            }
+
+            public boolean tipoTransportePorParada(String tipo) {
+                return tiposDeTransporte.contains(tipo);
+            }
+
+            public Set<String> getTiposDeTransporte() {
+                return tiposDeTransporte;
+            }
+
+
+
+
+
+
+
+    }
+
