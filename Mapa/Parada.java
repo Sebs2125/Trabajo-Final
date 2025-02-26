a51b5ee375aa904726db873648a78f97ba47a25d
EgnalyGonz <EXGC0003@ce.pucmm.edu.do>
2025-02-26 14:19:19 -0400

    elimine el la parte no usada de la clase Parada

--- Mapa/Parada.java
+++ Mapa/Parada.java
@@ -1,13 +1,15 @@
 import java.util.HashSet;
 import java.util.Set;
 
-/*public class Parada
-{
-    private String nombre;
 
-    public Parada ( String nombre )
-    {
+public class Parada {
+
+    private String nombre;
+    private Set<String> tiposDeTransporte; // agregue esto asi podemos manejar los tipos de transporte que habiamos hablado
+
+    public Parada(String nombre) {
         this.nombre = nombre;
+        this.tiposDeTransporte = new HashSet<>();
     }
 
     public String getNombre() {
@@ -17,44 +19,25 @@ import java.util.Set;
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    */
 
-        public class Parada {
+    public void agregarTransporte(String tipo) {
+        tiposDeTransporte.add(tipo);
+    }
 
-            private String nombre;
-            private Set<String> tiposDeTransporte; // agregue esto asi podemos manejar los tipos de transporte que habiamos hablado
+    public void eliminarTransporte(String tipo) {
+        tiposDeTransporte.remove(tipo);
+    }
 
-            public Parada(String nombre) {
-                this.nombre = nombre;
-                this.tiposDeTransporte = new HashSet<>();
-            }
+    public boolean tipoTransportePorParada(String tipo) {
+        return tiposDeTransporte.contains(tipo);
+    }
 
-            public String getNombre() {
-                return nombre;
-            }
-
-            public void setNombre(String nombre) {
-                this.nombre = nombre;
-            }
-
-            public void agregarTransporte(String tipo) {
-                tiposDeTransporte.add(tipo);
-            }
-
-            public void eliminarTransporte(String tipo) {
-                tiposDeTransporte.remove(tipo);
-            }
-
-            public boolean tipoTransportePorParada(String tipo) {
-                return tiposDeTransporte.contains(tipo);
-            }
-
-            public Set<String> getTiposDeTransporte() {
-                return tiposDeTransporte;
-            }
+    public Set<String> getTiposDeTransporte() {
+        return tiposDeTransporte;
+    }
 
 
-        }
+}
 
 
 
